f0578953b626704c65406719780780f62810812e
Juhász Márk <jmark45454@gmail.com>
2025-11-17 22:25:46 +0100

    Issue #21

--- scenes/objects/activatables/hook_barrier.gd
+++ scenes/objects/activatables/hook_barrier.gd
@@ -1,4 +1,4 @@
-class_name ToggleableTerrain
+class_name HookBarrier
 extends Activatable
 
 @export var preActivated: bool = false
@@ -7,31 +7,31 @@ func _ready() -> void:
 	if preActivated:
 		$StaticBody3D/CollisionShape3D.disabled = true
 		var tween = create_tween()
-		tween.tween_method(self.fade,1.0,0.25,1.0)
+		tween.tween_method(self.fade,0.25,0,1.0)
 
 func trigger() -> void:
 	if not preActivated:
 		activated = true
 		$StaticBody3D/CollisionShape3D.disabled = true
 		var tween = create_tween()
-		tween.tween_method(self.fade,1.0,0.25,1.0)
+		tween.tween_method(self.fade,0.25,0.0,1.0)
 	else:
 		activated = true
 		$StaticBody3D/CollisionShape3D.disabled = false
 		var tween = create_tween()
-		tween.tween_method(self.fade,0.25,1.0,1.0)
+		tween.tween_method(self.fade,0.25,0.0,1.0)
 
 func untrigger() -> void:
 	if not preActivated:
 		activated = false
 		$StaticBody3D/CollisionShape3D.disabled = false
 		var tween = create_tween()
-		tween.tween_method(self.fade,0.25,1.0,1.0)
+		tween.tween_method(self.fade,0.0,0.25,1.0)
 	else:
 		activated = false
 		$StaticBody3D/CollisionShape3D.disabled = true
 		var tween = create_tween()
-		tween.tween_method(self.fade,1.0,0.25,1.0)
+		tween.tween_method(self.fade,0.25,0.0,1.0)
 
 func fade(value: float) -> void:
 	$Mesh.mesh.material.albedo_color.a = value
--- scenes/objects/activatables/player_barrier.gd
+++ scenes/objects/activatables/player_barrier.gd
@@ -1,4 +1,4 @@
-class_name ToggleableTerrain
+class_name PlayerBarrier
 extends Activatable
 
 @export var preActivated: bool = false
@@ -6,32 +6,44 @@ extends Activatable
 func _ready() -> void:
 	if preActivated:
 		$StaticBody3D/CollisionShape3D.disabled = true
+		$Area3D/CollisionShape3D.disabled = true
 		var tween = create_tween()
-		tween.tween_method(self.fade,1.0,0.25,1.0)
+		tween.tween_method(self.fade,0.25,0,1.0)
 
 func trigger() -> void:
 	if not preActivated:
 		activated = true
 		$StaticBody3D/CollisionShape3D.disabled = true
+		$Area3D/CollisionShape3D.disabled = true
 		var tween = create_tween()
-		tween.tween_method(self.fade,1.0,0.25,1.0)
+		tween.tween_method(self.fade,0.25,0.0,1.0)
 	else:
 		activated = true
 		$StaticBody3D/CollisionShape3D.disabled = false
+		$Area3D/CollisionShape3D.disabled = false
 		var tween = create_tween()
-		tween.tween_method(self.fade,0.25,1.0,1.0)
+		tween.tween_method(self.fade,0.25,0.0,1.0)
 
 func untrigger() -> void:
 	if not preActivated:
 		activated = false
 		$StaticBody3D/CollisionShape3D.disabled = false
+		$Area3D/CollisionShape3D.disabled = false
 		var tween = create_tween()
-		tween.tween_method(self.fade,0.25,1.0,1.0)
+		tween.tween_method(self.fade,0.0,0.25,1.0)
 	else:
 		activated = false
 		$StaticBody3D/CollisionShape3D.disabled = true
+		$Area3D/CollisionShape3D.disabled = true
 		var tween = create_tween()
-		tween.tween_method(self.fade,1.0,0.25,1.0)
+		tween.tween_method(self.fade,0.25,0.0,1.0)
 
 func fade(value: float) -> void:
 	$Mesh.mesh.material.albedo_color.a = value
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if is_instance_of(body,Player) and body.is_hooking:
+		body.set_move_state("Jump_Idle")
+		body.squash_and_stretch_component.do_squash_and_stretch(1.2,0.15)
+		body.velocity.y = body.djump_impulse
+		body.is_hooking = false
